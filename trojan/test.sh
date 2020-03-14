dad9a769db42eb1b83555580d276d34310201108
troray <binghe27@gmail.com>
2020-03-14 11:18:48 +0800

    Create test.sh

---
+++ trojan/test.sh
@@ -0,0 +1,308 @@
+#!/bin/bash
+
+#fonts color
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+#copy from 秋水逸冰 ss scripts
+if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+    systemPackage="yum"
+    systempwd="/usr/lib/systemd/system/"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+    systemPackage="apt-get"
+    systempwd="/lib/systemd/system/"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+    systemPackage="apt-get"
+    systempwd="/lib/systemd/system/"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+    systemPackage="yum"
+    systempwd="/usr/lib/systemd/system/"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+    systemPackage="apt-get"
+    systempwd="/lib/systemd/system/"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+    systemPackage="apt-get"
+    systempwd="/lib/systemd/system/"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+    systemPackage="yum"
+    systempwd="/usr/lib/systemd/system/"
+fi
+
+function install_trojan(){
+CHECK=$(grep SELINUX= /etc/selinux/config | grep -v "#")
+if [ "$CHECK" == "SELINUX=enforcing" ]; then
+    red "======================================================================="
+    red "检测到SELinux为开启状态，为防止申请证书失败，请先重启VPS后，再执行本脚本"
+    red "======================================================================="
+    read -p "是否现在重启 ?请输入 [Y/n] :" yn
+	[ -z "${yn}" ] && yn="y"
+	if [[ $yn == [Yy] ]]; then
+	    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+            setenforce 0
+	    echo -e "VPS 重启中..."
+	    reboot
+	fi
+    exit
+fi
+if [ "$CHECK" == "SELINUX=permissive" ]; then
+    red "======================================================================="
+    red "检测到SELinux为宽容状态，为防止申请证书失败，请先重启VPS后，再执行本脚本"
+    red "======================================================================="
+    read -p "是否现在重启 ?请输入 [Y/n] :" yn
+	[ -z "${yn}" ] && yn="y"
+	if [[ $yn == [Yy] ]]; then
+	    sed -i 's/SELINUX=permissive/SELINUX=disabled/g' /etc/selinux/config
+            setenforce 0
+	    echo -e "VPS 重启中..."
+	    reboot
+	fi
+    exit
+fi
+if [ "$release" == "centos" ]; then
+    if  [ -n "$(grep ' 6\.' /etc/redhat-release)" ] ;then
+    red "==============="
+    red "当前系统不受支持"
+    red "==============="
+    exit
+    fi
+    if  [ -n "$(grep ' 5\.' /etc/redhat-release)" ] ;then
+    red "==============="
+    red "当前系统不受支持"
+    red "==============="
+    exit
+    fi
+    systemctl stop firewalld
+    systemctl disable firewalld
+    rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
+elif [ "$release" == "ubuntu" ]; then
+    if  [ -n "$(grep ' 14\.' /etc/os-release)" ] ;then
+    red "==============="
+    red "当前系统不受支持"
+    red "==============="
+    exit
+    fi
+    if  [ -n "$(grep ' 12\.' /etc/os-release)" ] ;then
+    red "==============="
+    red "当前系统不受支持"
+    red "==============="
+    exit
+    fi
+    systemctl stop ufw
+    systemctl disable ufw
+    apt-get update
+fi
+
+	#wget https://github.com/trojan-gfw/trojan/releases/download/v1.14.0/trojan-1.14.0-linux-amd64.tar.xz
+	wget https://github.com/trojan-gfw/trojan/releases/download/v1.14.1/trojan-1.14.1-linux-amd64.tar.xz
+	tar xf trojan-1.*
+	#下载trojan客户端
+	wget https://github.com/troray/trojan/raw/master/trojan-cli.zip
+	unzip trojan-cli.zip
+	trojan_passwd=$(cat /dev/urandom | head -1 | md5sum | head -c 8)
+	cat > /usr/src/trojan-cli/config.json <<-EOF
+{
+    "run_type": "client",
+    "local_addr": "127.0.0.1",
+    "local_port": 1080,
+    "remote_addr": "$your_domain",
+    "remote_port": 443,
+    "password": [
+        "$trojan_passwd"
+    ],
+    "log_level": 1,
+    "ssl": {
+        "verify": true,
+        "verify_hostname": true,
+        "cert": "fullchain.cer",
+        "cipher_tls13":"TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
+	"sni": "",
+        "alpn": [
+            "h2",
+            "http/1.1"
+        ],
+        "reuse_session": true,
+        "session_ticket": false,
+        "curves": ""
+    },
+    "tcp": {
+        "no_delay": true,
+        "keep_alive": true,
+        "fast_open": false,
+        "fast_open_qlen": 20
+    }
+}
+EOF
+	rm -rf /usr/src/trojan/server.conf
+	cat > /usr/src/trojan/server.conf <<-EOF
+{
+    "run_type": "server",
+    "local_addr": "0.0.0.0",
+    "local_port": 443,
+    "remote_addr": "127.0.0.1",
+    "remote_port": 80,
+    "password": [
+        "$trojan_passwd"
+    ],
+    "log_level": 1,
+    "ssl": {
+        "cert": "/www/server/panel/vhost/cert/www.troray.com/fullchain.pem",
+        "key": "/www/server/panel/vhost/cert/www.troray.com/privkey.pem",
+        "key_password": "",
+        "cipher_tls13":"TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
+	"prefer_server_cipher": true,
+        "alpn": [
+            "http/1.1"
+        ],
+        "reuse_session": true,
+        "session_ticket": false,
+        "session_timeout": 600,
+        "plain_http_response": "",
+        "curves": "",
+        "dhparam": ""
+    },
+    "tcp": {
+        "no_delay": true,
+        "keep_alive": true,
+        "fast_open": false,
+        "fast_open_qlen": 20
+    },
+    "mysql": {
+        "enabled": false,
+        "server_addr": "127.0.0.1",
+        "server_port": 3306,
+        "database": "trojan",
+        "username": "trojan",
+        "password": ""
+    }
+}
+EOF
+	
+	#增加启动脚本
+	
+cat > ${systempwd}trojan.service <<-EOF
+[Unit]  
+Description=trojan  
+After=network.target  
+   
+[Service]  
+Type=simple  
+PIDFile=/usr/src/trojan/trojan/trojan.pid
+ExecStart=/usr/src/trojan/trojan -c "/usr/src/trojan/server.conf"  
+ExecReload=  
+ExecStop=/usr/src/trojan/trojan  
+PrivateTmp=true  
+   
+[Install]  
+WantedBy=multi-user.target
+EOF
+
+	chmod +x ${systempwd}trojan.service
+	systemctl start trojan.service
+	systemctl enable trojan.service
+	green "======================================================================"
+	green "Trojan已安装完成，请使用以下链接下载trojan客户端，此客户端已配置好所有参数"
+	green "1、复制下面的链接，在浏览器打开，下载客户端"
+	yellow "http://${your_domain}/$trojan_path/trojan-cli.zip"
+	red "请记录下面规则网址"
+	yellow "http://${your_domain}/trojan.txt"
+	green "2、将下载的压缩包解压，打开文件夹，打开start.bat即打开并运行Trojan客户端"
+	green "3、打开stop.bat即关闭Trojan客户端"
+	green "4、Trojan客户端需要搭配浏览器插件使用，例如switchyomega等"
+	green "======================================================================"
+	else
+    red "================================"
+	red "https证书没有申请成功，本次安装失败"
+	red "================================"
+	fi
+	
+else
+	red "================================"
+	red "域名解析地址与本VPS IP地址不一致"
+	red "本次安装失败，请确保域名解析正常"
+	red "================================"
+fi
+}
+
+function remove_trojan(){
+    red "================================"
+    red "即将卸载trojan"
+    red "================================"
+    systemctl stop trojan
+    systemctl disable trojan
+    rm -f ${systempwd}trojan.service
+    if [ "$release" == "centos" ]; then
+       
+    else
+        
+    fi
+    rm -rf /usr/src/trojan*
+    
+    green "=============="
+    green "trojan删除完毕"
+    green "=============="
+}
+
+function bbr_boost_sh(){
+    wget --no-check-certificate -O tcp.sh https://raw.githubusercontent.com/troray/Linux-NetSpeed/master/tcp.sh && chmod +x tcp.sh && ./tcp.sh
+}
+
+start_menu(){
+    clear
+    green " ===================================="
+    green " Trojan 一键安装自动脚本      "
+    green " 系统：centos7+/debian8/9+/ubuntu16.04+"
+    green " BBR脚本：https://github.com/cx9208"
+    green " Trojan脚本：https://github.com/atrandys"
+    green " WEB来自https://github.com/geekape"
+    green " 脚本由波仔集成；本人只是升级了Trojan本版 "
+    green " 以及修改了BBR安装方式。                "
+    green " ===================================="
+    echo
+    red " ===================================="
+    yellow " 1. 一键安装 Trojan"
+    red " ===================================="
+    yellow " 2. 一键卸载 Trojan"
+    red " ===================================="
+    yellow " 3. 安装 4 IN 1 BBRPLUS加速脚本"
+    red " ===================================="
+    yellow " 0. 退出脚本"
+    red " ===================================="
+    echo
+    read -p "请输入数字:" num
+    case "$num" in
+    1)
+    install_trojan
+    ;;
+    2)
+    remove_trojan 
+    ;;
+    3)
+    bbr_boost_sh
+    ;;
+    0)
+    exit 1
+    ;;
+    *)
+    clear
+    red "请输入正确数字"
+    sleep 1s
+    start_menu
+    ;;
+    esac
+}
+
+start_menu
